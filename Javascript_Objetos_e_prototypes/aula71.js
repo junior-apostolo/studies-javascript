591ab5ef365b018536c0756cb3929a66ebf3528c
Sergio Borges <sergio.borgesjunior@everis.nttdata.com>
2024-05-28 16:08:02 -0300

    define.Property and define.Properties

---
+++ Javascript_Objetos_e_prototypes/aula71.js
@@ -0,0 +1,32 @@
+function Produto(nome, preco, estoque) {
+  // define uma nova propriedade diretamente em um objeto, 
+  // ou modifica uma propriedade já existente em um objeto, e retorna o objeto.
+  Object.defineProperty(this, "estoque", {
+    enumerable: true, // mostra a chave
+    value: estoque, // valor
+    writable: false, // pode alterar ou não
+    configurable: false, // permite ou não reconfigurar o atributo Ex: delete
+  });
+
+  Object.defineProperties(this, {
+    nome: {
+      enumerable: true, // mostra a chave
+      value: nome, // valor
+      writable: false, // pode alterar ou não
+      configurable: false, // permite ou não reconfigurar o atributo Ex: delete
+    },
+    preco: {
+      enumerable: true, // mostra a chave
+      value: preco, // valor
+      writable: false, // pode alterar ou não
+      configurable: false, // permite ou não reconfigurar o atributo Ex: delete
+    },
+  });
+}
+
+const p1 = new Produto("Camisa", 22, 5);
+console.log(p1);
+
+for(let chave in p1){
+    console.log(chave)
+}
