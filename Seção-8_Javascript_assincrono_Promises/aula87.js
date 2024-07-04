f00497a54eee824a6b8e9e68a31494dc3e20c0be
junior-apostolo <juniorapostolo2010@gmail.com>
2024-07-03 22:15:50 -0300

    feat: Promises in practice (All, Race)

--- Seção-8_Javascript_assincrono_Promises/aula87.js
+++ Seção-8_Javascript_assincrono_Promises/aula87.js
@@ -6,13 +6,34 @@ function rand(min, max) {
 
 function esperaAi(msg, tempo) {
   return new Promise((resolve, reject) => {
-    if (typeof msg !== "string") reject(new Error("Erro!!!"));
-
     setTimeout(() => {
+      if (typeof msg !== "string") reject(new Error("Erro!!!"));
       resolve(msg);
     }, tempo);
   });
 }
 
+const promises = [
+  esperaAi("Promise 1", rand(1, 5)),
+  esperaAi("Promise 2", rand(1, 5)),
+  esperaAi("Promise 3", rand(1, 5)),
+  esperaAi(1000, rand(1, 5)),
+];
+
 // Promise.all -> resolve todas as promises. Porém se uma cair no erro ele retorna somente o erro.
+Promise.all(promises)
+  .then((response) => {
+    console.log(response + " PROMISES ALL");
+  })
+  .catch((erro) => {
+    console.log(erro);
+  });
+
 // Promise.race -> entrega sempre o primeiro valor/promise resolvido.
+Promise.race(promises)
+  .then((response) => {
+    console.log(response + " PROMISES RACE");
+  })
+  .catch((erro) => {
+    console.log(erro);
+  });
